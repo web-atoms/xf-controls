55965c863a622157d763366872df9eaf0f5ec2a7
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-01-28 13:24:50 +0530

    wip

--- src/AtomComboBox.ts
+++ src/AtomComboBox.ts
@@ -41,6 +41,7 @@ export default class Root extends AtomXFControl {
 
     public readonly items: AtomList<any> = new AtomList<any>();
 
+    @BindableProperty
     public itemsSource: (fetchEvent: IFetchEvent) => Promise<any[]> | any[] = null;
 
     public valueFunc: ((item: any) => any) = null;
