f11a966bfb5b3376e11152e00b6f4f9c2d4ef5f2
Akash Kava <ackava@gmail.com>
2019-12-26 14:11:12 +0530

    Added grid support

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,9 +1,43 @@
+import Bind from "@web-atoms/core/dist/core/xnode/Bind";
+import XNode from "@web-atoms/core/dist/core/xnode/XNode";
 import { AtomXFControl } from "@web-atoms/core/dist/xf/controls/AtomXFControl";
+import { PopupPage } from "./controls/RgPluginsPopup";
+import { ColumnDefinition, Entry, Grid, ListView, RowDefinition } from "./controls/XF";
+import SearchPageViewModel from "./view-models/SearchPageViewModel";
 
 export default class SearchPage extends AtomXFControl {
 
+	public viewModel: SearchPageViewModel;
+
 	public create() {
-		this.render()
+
+		this.viewModel = this.resolve(SearchPageViewModel);
+
+		this.render(<PopupPage>
+			<Grid
+				margin="10"
+				backgroundColor="White"
+				rowDefinitions={[
+					<RowDefinition height="Auto"/>,
+					<RowDefinition/>]}
+				columnDefinitions={[
+					<ColumnDefinition/>,
+					<ColumnDefinition width="Auto"/>
+				]}
+				>
+				<Entry
+					text={Bind.twoWays(() => this.viewModel.comboBox.searchText)}
+					isVisible={Bind.oneWay(() => this.viewModel.comboBox.searchEnabled)}/>
+				<ListView
+					{ ... {
+						"Grid.Row": 1,
+						"Grid.ColumnSpan": 2
+					}}
+					cachingStrategy="RecycleElement"
+					itemsSource={Bind.oneTime(() => this.viewModel.comboBox.items)}
+					itemTemplate={Bind.oneTime(() => this.viewModel.comboBox.itemTemplate)}/>
+			</Grid>
+		</PopupPage>);
 	}
 
 }
---
+++ src/controls/RgPluginsPopup.ts
@@ -0,0 +1,4 @@
+import XNode from "@web-atoms/core/dist/core/xnode/XNode";
+import { IContentPage } from "./XF";
+
+export const PopupPage = XNode.prepare<IContentPage>("PopupPage|clr-namespace:Rg.Plugins.Popup.Pages;assembly=Rg.Plugins.Popup");
