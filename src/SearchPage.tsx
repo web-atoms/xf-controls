d316082d39ab18ad62fb0da44da22808e9357073
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-12-26 12:36:16 +0530

    wip

---
+++ src/SearchPage.tsx
@@ -0,0 +1,9 @@
+import { AtomXFControl } from "@web-atoms/core/dist/xf/controls/AtomXFControl";
+
+export default class SearchPage extends AtomXFControl {
+
+	public create() {
+		this.render()
+	}
+
+}
