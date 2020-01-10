e6e6407d04dce65e95e9464e6f26291ca059c1bd
Akash Kava <ackava@gmail.com>
2020-01-10 15:18:42 +0530

    Added AtomX

---
+++ src/controls/X.ts
@@ -0,0 +1,8 @@
+import XNode from "@web-atoms/core/dist/core/XNode";
+
+const X = {
+    Name: (n: string) => ({ "WebAtoms.AtomX:Name": n }),
+    Key: (n: string) => ({ "WebAtoms.AtomX:Key": n }),
+};
+
+export default X;
