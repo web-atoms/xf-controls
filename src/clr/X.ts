4ed2d7f9f2b8b893ff35e82692b70429485b62e8
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-01-27 11:49:39 +0530

    wipp

--- src/clr/X.ts
+++ src/clr/X.ts
@@ -2,7 +2,7 @@ import XNode from "@web-atoms/core/dist/core/XNode";
 
 const X = {
     Name: (n: string) => ({ "WebAtoms.AtomX:Name": n }),
-    Key: (n: string) => ({ "WebAtoms.AtomX:Key": n }),
+    // Key: (n: string) => ({ "WebAtoms.AtomX:Key": n }),
 };
 
 export default X;
