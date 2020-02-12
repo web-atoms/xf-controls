8ea8e26125410fafeebdba7894c4e2af3176fe0f
Akash Kava <ackava@gmail.com>
2020-02-12 14:15:48 +0530

    wip

--- src/clr/X.ts
+++ src/clr/X.ts
@@ -2,6 +2,7 @@ import XNode from "@web-atoms/core/dist/core/XNode";
 
 const X = {
     Name: (n: string) => ({ "WebAtoms.AtomX:Name": n }),
+    Arguments: ( args?: any, ... nodes: XNode[]) => new XNode("WebAtoms.AtomX:Arguments", {}, nodes),
     // Key: (n: string) => ({ "WebAtoms.AtomX:Key": n }),
 };
 
