2cea05d59c93656f971e010c400875e42d02e6b1
Akash Kava <ackava@gmail.com>
2020-02-12 17:37:27 +0530

    Added pages

---
+++ src/pages/AtomXFTabbedPage.ts
@@ -0,0 +1,11 @@
+import { App } from "@web-atoms/core/dist/App";
+import { AtomBridge } from "@web-atoms/core/dist/core/AtomBridge";
+import { AtomXFControl } from "@web-atoms/core/dist/xf/controls/AtomXFControl";
+import XF from "../clr/XF";
+
+export default class AtomXFTabbedPage extends AtomXFControl {
+
+    constructor(app: App, e?: any) {
+        super(app, e || AtomBridge.instance.create(XF.TabbedPage));
+    }
+}
