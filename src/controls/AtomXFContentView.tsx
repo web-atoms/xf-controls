a0fbcbf812dfa7cc265fb1fcaa2176c14c6d3064
Akash Kava <ackava@gmail.com>
2020-02-13 17:31:49 +0530

    Added ContentView

---
+++ src/controls/AtomXFContentView.tsx
@@ -0,0 +1,11 @@
+import { AtomBridge } from "@web-atoms/core/dist/core/AtomBridge";
+import { AtomXFControl } from "@web-atoms/core/dist/xf/controls/AtomXFControl";
+import XF from "../clr/XF";
+
+export default class AtomXFContentView extends AtomXFControl {
+
+    constructor(app: any, e?: any) {
+        super(app, e || AtomBridge.instance.create(XF.ContentView));
+    }
+
+}
