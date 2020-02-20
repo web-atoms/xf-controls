be9963de3fb6b8110497ba3bda413cc79d5d4768
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-02-20 08:37:49 +0530

    wwip

--- src/clr/X.ts
+++ src/clr/X.ts
@@ -1,8 +1,34 @@
 import XNode from "@web-atoms/core/dist/core/XNode";
+import Bind from "@web-atoms/core/dist/core/Bind";
+
+function RelativeSource(
+    mode: "FindAncestor" | "FindAncestorBindingContext",
+    ancestorType: string | any) {
+    if (ancestorType.factory) {
+        ancestorType = ancestorType.toString();
+    }
+    return {
+        mode,
+        ancestorType
+    };
+}
+
+RelativeSource.self = { mode: "Self" };
+RelativeSource.TemplatedParent = { mode: "TemplatedParent" };
 
 const X = {
     Name: (n: string) => ({ "WebAtoms.AtomX:Name": n }),
     Arguments: ( args?: any, ... nodes: XNode[]) => new XNode("WebAtoms.AtomX:Arguments", {}, nodes),
+    Type: (n: string) => ({ type: n }),
+    Resource: (n: string) => ({ resource: n}),
+    RelativeSource,
+    Binding: (b: {
+        path: string,
+        source: any,
+        converter: any,
+        converterParameter: any}) => {
+            return Bind.oneTime((c, e) => null);
+        }
     // Key: (n: string) => ({ "WebAtoms.AtomX:Key": n }),
 };
 
