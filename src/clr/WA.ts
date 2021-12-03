d62b80c47f19c4da37090e27a4c58b8b20bc55d9
Akash Kava <ackava@gmail.com>
2021-12-03 16:50:58 +0530

    wip

--- src/clr/WA.ts
+++ src/clr/WA.ts
@@ -280,6 +280,7 @@ namespace WA {
         public fill: XF.Brush | Bind;
         public secondaryTextColor: XF.Color | Bind;
         public secondaryOpacity: number | Bind;
+        public secondaryText: string;
         public fontSize: number;
         public fontFamily: string;
     }
